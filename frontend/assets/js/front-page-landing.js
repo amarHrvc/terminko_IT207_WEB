bcf63cd1e8fcb472d9f7256b4062b58c0f72010c
amar.hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2025-03-20 21:06:23 +0100

    [Add] extract menu to partial

--- frontend/assets/js/front-page-landing.js
+++ frontend/assets/js/front-page-landing.js
@@ -21,6 +21,7 @@
   // Hero
   const mediaQueryXL = '1200';
   const width = screen.width;
+  console.log("🚀 ~  ~ width: ", width);
   if (width >= mediaQueryXL && heroAnimation) {
     heroAnimation.addEventListener('mousemove', function parallax(e) {
       animationElements.forEach(layer => {
@@ -32,16 +33,7 @@
         layer.style.transform = `perspective(1200px) rotateX(${y}deg) rotateY(${x}deg) scale3d(1, 1, 1)`;
       });
     });
-    nav.addEventListener('mousemove', function parallax(e) {
-      animationElements.forEach(layer => {
-        layer.style.transform = 'translateZ(1rem)';
-      });
-      animationImg.forEach(layer => {
-        let x = (window.innerWidth - e.pageX * 2) / 100;
-        let y = (window.innerHeight - e.pageY * 2) / 100;
-        layer.style.transform = `perspective(1200px) rotateX(${y}deg) rotateY(${x}deg) scale3d(1, 1, 1)`;
-      });
-    });
+
 
     heroAnimation.addEventListener('mouseout', function () {
       animationElements.forEach(layer => {
