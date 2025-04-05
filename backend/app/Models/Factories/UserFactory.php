64bdc229a834f28c3369d6b3052e83270bf1eda8
amar.hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2025-04-05 13:29:51 +0200

    [Add] Tenant, User models and daos
    Tenant,  user models and factories
    baseDao, Tenant and user daos

---
+++ backend/app/Models/Factories/UserFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Factories;
+
+class UserFactory
+{
+    public function create($data)
+    {
+        $user = new User(
+            $data['id'] ?? null,
+            $data['tenant_id'] ?? null,
+            $data['name'] ?? null,
+            $data['email'] ?? null,
+            $data['password_hash'] ?? null,
+            $data['phone'] ?? null,
+            $data['role'] ?? null,
+            $data['created_at'] ?? null,
+            $data['updated_at'] ?? null
+        );
+
+        return $user;
+    }
+
+}
