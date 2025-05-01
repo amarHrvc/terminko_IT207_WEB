a47c20b752861bc0f298aa65c1d09847748300cd
amar.hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2025-05-01 23:30:11 +0200

    [Add] DOtENv added

--- backend/app/Database/Database.php
+++ backend/app/Database/Database.php
@@ -17,11 +17,10 @@ class Database
     {
         if (self::$instance === null) {
             try {
-                // TODO: Move these to environment variables
-                $host = 'localhost';
-                $dbname = 'terminko';
-                $username = 'root';
-                $password = '';
+                $host = $_ENV['DB_HOST'] ?? 'localhost';
+                $dbname = $_ENV['DB_NAME'] ?? 'terminko';
+                $username = $_ENV['DB_USER'] ?? 'root';
+                $password = $_ENV['DB_PASS'] ?? '';
 
                 self::$instance = new PDO(
                     "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
