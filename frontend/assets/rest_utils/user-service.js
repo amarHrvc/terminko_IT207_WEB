eb78ce67ac8d3403777a174680dbf820c5a4ff0c
amar.hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2025-05-25 07:40:40 +0200

    [Add] jquery restclient login added

---
+++ frontend/assets/rest_utils/user-service.js
@@ -0,0 +1,57 @@
+let UserService = {
+    init: function () {
+        var token = localStorage.getItem("user_token");
+        if (token && token !== undefined) {
+            // window.location.replace("index.html");
+        }
+
+        let form = $("#formAuthentication");
+        console.log('FOOOORMMMMMMMMMMMMMMMMM', form);
+
+        $("#formAuthentication").on('submit', function (e) {
+            e.preventDefault();
+            let formEntity = Object.fromEntries(new FormData(this));
+            console.log('++++FormEntity++++', formEntity);
+
+            if (!formEntity.email || !formEntity.password) {
+                alert("Fill in all fealds !");
+                return;
+            }
+
+            UserService.login(formEntity);
+        });
+    },
+
+    login: function (entity) {
+        // $.ajax({
+        //     url: API_BASE_URL + "login",
+        //     type: "POST",
+        //     data: JSON.stringify(entity),
+        //     contentType: "application/json",
+        //     dataType: "json",
+        //     success: function (result) {
+        //         console.log("RESUUUULTTT ::::", result);
+        //         localStorage.setItem("user_token", result.token);
+        //         window.location.replace("#dashboard");
+        //     },
+        //     error: function (xhr, textStatus, errorThrown) {
+        //         // toastr.error(xhr?.responseText ? xhr.responseText : 'Error');
+        //         console.error('xhr.responseJSON:', xhr.responseJSON);
+        //         console.error('textStatus:', textStatus);
+        //         console.error('errorThrown:', errorThrown);
+        //     },
+        // });
+        //
+        RestClient.requestWprom("login", "POST", entity)
+            .done(response => {
+                console.log("++++ requestWprom +++++++", response);
+                window.location.replace("#dashboard");
+            })
+            .fail(xhr => console.log(xhr.responseJSON?.error));
+    },
+
+    logout: function () {
+        localStorage.clear();
+        window.location.replace("login.html");
+    }
+}
