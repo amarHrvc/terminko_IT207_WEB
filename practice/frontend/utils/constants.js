1123110a449f6d1efe361e9627382b9cd728c2df
amar.hajrovic <amar.hajrovic@stu.ibu.edu.ba>
2025-07-02 23:33:47 +0200

    [Fix] localhost fix

--- practice/frontend/utils/constants.js
+++ practice/frontend/utils/constants.js
@@ -1,9 +1,9 @@
 var Constants = {
   get_api_base_url: function () {
     if(location.hostname == 'localhost'){
-      return "http://practice.test/backend/rest";
+      return "http://localhost/practice/backend/rest";
     } else {
-      return "http://practice.test/backend/rest";
+      return "http://localhost/practice/backend/rest";
     }
   }
 };
